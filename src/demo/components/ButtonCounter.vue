74fc19769af9a59d2ac9a7958b224ba09c80a9af
haohua <haohua@skyable.cn>
2024-09-14 17:34:24 +0800

    vue basic demo

---
+++ src/demo/components/ButtonCounter.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>{{ title }}</p>
+    <button @click="count++">{{ count }}</button>
+</template>
+<script setup >
+import {ref} from 'vue'
+const count = ref(0);
+
+defineProps(['title'])
+
+
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Graph from '../components/GraphDemo.vue'
+import DEMO1 from '../demo/1_options.vue'
+import DEMO2 from '../demo/2_composition.vue'
+import DEMO3 from '../demo/3_component.vue'
 
 const routes = [
   { path: '/', component: Graph}, 
-  { path: '/demo', component: Graph}, 
+  { path: '/demo/1', component: DEMO1}, 
+  { path: '/demo/2', component: DEMO2}, 
+  { path: '/demo/3', component: DEMO3}, 
 ];
 
 const router = createRouter({
